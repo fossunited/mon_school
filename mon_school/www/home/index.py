c353a237e014b05cd31ba1f463685cece9639fea
Anand Chitipothu <anandology@gmail.com>
2021-07-17 09:26:12 +0530

    Removed the portal page for home.

--- mon_school/www/home/index.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from community.lms.models import Course
-from mon_school.mon_school.doctype.lms_sketch.lms_sketch import LMSSketch as Sketch
-
-
-def get_context(context):
-	context.no_cache = 1
-	context.courses = Course.find_all()
-	context.recent_sketches = Sketch.get_recent_sketches(limit=8)
