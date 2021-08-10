3785a7bdd4c155e40727e1581f2ad4093bbf18f2
Anand Chitipothu <anandology@gmail.com>
2021-08-10 09:15:29 +0530

    feat: added API endpoint to join a contest

--- mon_school/mon_school/doctype/contest/contest.py
+++ mon_school/mon_school/doctype/contest/contest.py
@@ -1,8 +1,40 @@
 # Copyright (c) 2021, FOSS United and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Contest(Document):
-	pass
+    def is_participant(self, email):
+        return frappe.db.exists({
+            'doctype': "Contest Participant",
+            'contest': self.name,
+            'participant': email
+        })
+
+    def add_participant(self, email):
+        if self.is_participant(email):
+            return
+        doc = frappe.get_doc(
+            doctype="Contest Participant",
+            contest=self.name,
+            participant=email)
+        doc.insert(ignore_permissions=True)
+
+    def remove_participant(self, email):
+        filters = {
+            'contest': self.name,
+            'participant': email
+        }
+        doc = frappe.get_doc("Contest Participant", filters=filters)
+        if doc:
+            doc.delete()
+
+@frappe.whitelist()
+def join_contest(contest_name):
+    contest = frappe.get_doc("Contest", contest_name)
+    if contest:
+        contest.add_participant(frappe.session.user)
+        return {"ok": True}
+    else:
+        return {"ok": False, "error": "Contest Not Found"}
