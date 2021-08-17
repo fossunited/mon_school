a9ea19a005affcf9ee9d11b7e8a294711a945ecb
Anand Chitipothu <anandology@gmail.com>
2021-08-17 12:46:52 +0530

    feat: remove the grid in the sketches when rendering as image

--- mon_school/mon_school/livecode.py
+++ mon_school/mon_school/livecode.py
@@ -83,6 +83,11 @@ def record_code_run(code, result, context=None):
 def livecode_to_svg(code, is_sketch=False):
     """Renders the code as svg.
     """
+    # XXX-Anand: Aug 2021
+    # hack to remove the grid from the generated images
+    setup_code = "show.__globals__['BG_SHOWN'] = 1\n\n"
+    code = setup_code + code
+
     result = execute(code, is_sketch=is_sketch)
     if result.get('status') != 'success':
         return None
