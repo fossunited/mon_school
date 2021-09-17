42b310c2da0c839b95dad7a0e6c8afdc32378288
Anand Chitipothu <anandology@gmail.com>
2021-09-17 17:05:27 +0530

    feat: allow fetching more sketches on sketches page by passing limit
    This is hack to see more of the until we add pagination.

--- mon_school/www/sketches/index.py
+++ mon_school/www/sketches/index.py
@@ -3,5 +3,22 @@ from mon_school.mon_school.doctype.lms_sketch.lms_sketch import LMSSketch as Ske
 
 def get_context(context):
     context.no_cache = 1
-    context.sketches = Sketch.get_recent_sketches(limit=48)
 
+    limit = get_limit()
+    context.sketches = Sketch.get_recent_sketches(limit=limit)
+
+def get_limit(default=48):
+    try:
+        value = frappe.form_dict.get("limit", default)
+        limit = int(value)
+        return bound(limit, min_value=default, max_value=1000)
+    except ValueError:
+        return default
+
+def bound(value, min_value, max_value):
+    if value < min_value:
+        return min_value
+    elif value > max_value:
+        return max_value
+    else:
+        return value
