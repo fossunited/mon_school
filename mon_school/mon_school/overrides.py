cc9b196961d0de3b879a7944cb6d7763ada47c8d
Anand Chitipothu <anandology@gmail.com>
2021-12-10 00:04:50 +0530

    replace get_students_with_stats with get_students_with_scores

--- mon_school/mon_school/overrides.py
+++ mon_school/mon_school/overrides.py
@@ -23,41 +23,18 @@ class LMSBatchMembership(_LMSBatchMembership):
             return super().validate_membership_in_different_batch_same_course()
 
 class CohortSubgroup(_CohortSubgroup):
-    def get_students_with_stats(self):
+    def get_students_with_score(self):
         students = self.get_students()
-        exercises = self.get_exercise_counts()
-        runs = self.get_run_counts(students)
-        sketches = self.get_sketch_counts(students)
+        scores = self.get_scores()
 
         for s in students:
-            s.num_exercises = exercises.get(s.name, 0)
-            s.num_runs = runs.get(s.name, 0)
-            s.num_sketches = sketches.get(s.name, 0)
+            s.score = scores.get(s.name, 0)
+        return sorted(students, key=lambda s: s.score, reverse=True)
 
-        return students
-
-    def get_exercise_counts(self):
-        rows = frappe.get_all("Exercise Latest Submission",
-            filters={"member_subgroup": self.name},
-            fields=["member_email", "count(*) as count"],
-            group_by="member_email",
+    def get_scores(self):
+        rows = frappe.get_all("Student Score Activity",
+            filters={"subgroup": self.name},
+            fields=["user", "sum(score) as score"],
+            group_by="user",
             page_length=1000)
         return {row.member_email: row.count for row in rows}
-
-    def get_run_counts(self, students):
-        emails = [s.name for s in students]
-        rows = frappe.get_all("Code Run",
-            filters={"owner": ["IN", emails]},
-            fields=["owner", "count(*) as count"],
-            group_by="owner",
-            page_length=1000)
-        return {row.owner: row.count for row in rows}
-
-    def get_sketch_counts(self, students):
-        emails = [s.name for s in students]
-        rows = frappe.get_all("LMS Sketch",
-            filters={"owner": ["IN", emails]},
-            fields=["owner", "count(*) as count"],
-            group_by="owner",
-            page_length=1000)
-        return {row.owner: row.count for row in rows}
