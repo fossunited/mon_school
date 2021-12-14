4513f1ddcb2cda4ae5a1edad5ad88dacf02b055e
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-12-14 21:06:22 +0530

    Add pagination for sketches

--- mon_school/www/sketches/index.py
+++ mon_school/www/sketches/index.py
@@ -1,11 +1,33 @@
+import math
+
 import frappe
 from mon_school.mon_school.doctype.lms_sketch.lms_sketch import LMSSketch as Sketch
+
 
 def get_context(context):
     context.no_cache = 1
 
+    page = get_page()
     limit = get_limit()
-    context.sketches = Sketch.get_recent_sketches(limit=limit)
+
+    num_sketches = Sketch.get_total_count()
+
+    start = get_offset(page, page_length=limit)
+    num_pages = get_num_pages(num_sketches, page_length=limit)
+
+    context.page = page
+    context.num_pages = num_pages
+    context.sketches = Sketch.get_recent_sketches(start=start, limit=limit)
+
+
+def get_page():
+    try:
+        page = int(frappe.form_dict.get("page", 1))
+    except ValueError:
+        page = 1
+
+    return page
+
 
 def get_limit(default=48):
     try:
@@ -15,6 +37,7 @@ def get_limit(default=48):
     except ValueError:
         return default
 
+
 def bound(value, min_value, max_value):
     if value < min_value:
         return min_value
@@ -22,3 +45,12 @@ def bound(value, min_value, max_value):
         return max_value
     else:
         return value
+
+
+def get_offset(page, page_length):
+    return (page - 1) * page_length
+
+
+def get_num_pages(total_items, page_length):
+    # max(1, ...) so that there is at least 1 page, even with 0 items
+    return max(1, math.ceil(total_items/page_length))
