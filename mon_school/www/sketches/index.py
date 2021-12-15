4c87d9624028f0aa233e4be1c190c4dabe5134f3
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-12-15 14:24:10 +0530

    Sketches Pagination: handle case with too many pages

--- mon_school/www/sketches/index.py
+++ mon_school/www/sketches/index.py
@@ -19,6 +19,15 @@ def get_context(context):
     context.num_pages = num_pages
     context.sketches = Sketch.get_recent_sketches(start=start, limit=limit)
 
+    page_range_start, page_range_end = get_page_range(
+        current_page=page,
+        num_pages=num_pages,
+        window_size=5,
+    )
+
+    context.page_range_start = page_range_start
+    context.page_range_end = page_range_end
+
 
 def get_page():
     try:
@@ -54,3 +63,15 @@ def get_offset(page, page_length):
 def get_num_pages(total_items, page_length):
     # max(1, ...) so that there is at least 1 page, even with 0 items
     return max(1, math.ceil(total_items/page_length))
+
+
+def get_page_range(current_page, num_pages, window_size=5):
+    start = max(1, current_page-(window_size//2))
+    end = min(num_pages+1, start+window_size)
+
+    if end - start < window_size:
+        # when current_page is towards the end of num_pages,
+        # try to expand the window towards the left side
+        start = max(1, end-window_size)
+
+    return start, end
