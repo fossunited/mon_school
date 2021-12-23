8cc4206d7886a3853dc64eab3acb7bd57c66de8a
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2021-12-23 17:29:59 +0530

    Move index setting for fields to JSON instead of python code

--- mon_school/mon_school/doctype/lms_sketch_like/lms_sketch_like.py
+++ mon_school/mon_school/doctype/lms_sketch_like/lms_sketch_like.py
@@ -22,10 +22,6 @@ def on_doctype_update():
     # only one like for each sketch/user
     frappe.db.add_unique("LMS Sketch Like", fields=["sketch", "user"])
 
-    # index for faster queries
-    frappe.db.add_index("LMS Sketch Like", fields=["sketch"])
-    frappe.db.add_index("LMS Sketch Like", fields=["user"])
-
 
 @frappe.whitelist()
 def toggle_sketch_like(sketch_name, action):
