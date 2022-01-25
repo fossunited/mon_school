c998b4364b03026f7202b134e2e0d02a5d1a778d
Anand Chitipothu <anandology@gmail.com>
2022-01-25 05:34:26 +0530

    Added ability to attach an IP adddress to the user subdomain.
    Issue #91

---
+++ mon_school/www/account/subdomain.py
@@ -0,0 +1,52 @@
+from re import L
+from shutil import ignore_patterns
+import frappe
+
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/account/subdomain"
+        raise frappe.Redirect
+
+    if frappe.request.method == "POST":
+        try:
+            save_subdomain(frappe.form_dict.get("ip"))
+        except frappe.exceptions.ValidationError as e:
+            context.message = str(e)
+            context.message_type = "danger"
+        else:
+            context.message = "Successfully updated the IP address of your subdomain."
+            context.message_type = "success"
+
+    context.subdomain = get_subdomain()
+
+def get_subdomain():
+    doctype = "Mon School User Subdomain"
+    name = frappe.db.exists(doctype, {"user": frappe.session.user})
+    if name:
+        return frappe.get_doc(doctype, name)
+    else:
+        user = frappe.get_doc("User", frappe.session.user)
+        return frappe.get_doc({
+            "doctype": doctype,
+            "user": user.name,
+            "subdomain": user.username,
+            "ip": ""
+        })
+
+def save_subdomain(ip):
+    doctype = "Mon School User Subdomain"
+    name = frappe.db.exists(doctype, {"user": frappe.session.user})
+    user = frappe.get_doc("User", frappe.session.user)
+    if name:
+        doc = frappe.get_doc(doctype, name)
+        doc.ip = ip
+        doc.subdomain = user.username
+        doc.save("ignore_permissions", True)
+    else:
+        doc = frappe.get_doc({
+            "doctype": doctype,
+            "user": user.name,
+            "subdomain": user.username,
+            "ip": ip
+        })
+        doc.insert(ignore_permissions=True)
