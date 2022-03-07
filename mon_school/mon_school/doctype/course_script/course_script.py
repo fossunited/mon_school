bca287b6680988c3c99bedc964025a9abc3c92c7
Anand Chitipothu <anandology@gmail.com>
2022-03-07 16:31:16 +0530

    Added new doctype "Course Script" to inject custom scripts and styles

---
+++ mon_school/mon_school/doctype/course_script/course_script.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, FOSS United and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class CourseScript(Document):
+	pass
