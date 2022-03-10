f1950864d09980bd174083904b162ad54fd3e6d7
Anand Chitipothu <anandology@gmail.com>
2022-03-10 12:38:25 +0530

    Added new doctype Course Image to add images to lessons

---
+++ mon_school/mon_school/doctype/course_image/test_course_image.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, FOSS United and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCourseImage(FrappeTestCase):
+	pass
